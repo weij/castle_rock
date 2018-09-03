0036a50355f9ffdec0ea4865c8117d08e90ad637
Weijun Yin <weijun.yin@letote.cn>
2018-09-03 08:13:50 +0800

    add date_in_string function

--- lib/castle_rock.ex
+++ lib/castle_rock.ex
@@ -2,17 +2,20 @@ defmodule CastleRock do
   @moduledoc """
   Documentation for CastleRock.
   """
+  import StreamData
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CastleRock.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def date_in_string() do
+    StreamData.bind(integer(1976..9999), fn year ->
+      StreamData.bind(integer(1..12), fn month ->
+        StreamData.bind(integer(1..31), fn date ->
+          Date.from_erl({year, month, date})
+        end)
+      end)
+    end)
+    |> StreamData.filter(fn
+      {:ok, _date} -> true
+      _ -> false
+    end)
+    |> StreamData.map(fn {:ok, date} -> Date.to_string(date) end)
   end
 end
